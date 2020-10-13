2023f394e7b4c744c712b0f29ce5b25601cfc66b
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-10-13 19:59:58 +0300

    Created New Inventory listener and refactor events. Events MUST be under the same package across services for JMS!!! You can get around this restriction but it might impact performance.

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,32 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+
+//    Notice that we refactored and imported the event from the same package.
+//    This is required by JMS !!!
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void lister(NewInventoryEvent event){
+        log.debug("Got inventory: " + event.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build());
+    }
+}
